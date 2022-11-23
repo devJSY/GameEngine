43cdd4163a3964f03dc47e7e818b6813db2a3293
승연 정 <wjdtmd486@naver.com>
2022-11-23 20:51:52 +0900

    Collider 의 FinalPos 로는 Ground 충돌이 2개이상 발생되었을때 Pos 변경이 다음 프레임에 발생됨으로 Pos 로 설정

--- kirby/GameEngine/CGround.cpp
+++ kirby/GameEngine/CGround.cpp
@@ -47,10 +47,13 @@ void CGround::OnCollisionEnter(CCollider* _pOther)
 
 	if (pOtherObj->GetName() == L"Kirby")
 	{
-		Vec2 vObjPos = _pOther->GetFinalPos();
+		// Collider 의 FinalPos 로는 Ground 충돌이 2개이상 발생되었을때 Pos 변경이 다음 프레임에 발생됨으로 Pos 로 설정
+		Vec2 vObjPos = pOtherObj->GetPos();
+		vObjPos += ((CCollider*)pOtherObj->GetComponents(Component_TYPE::Collider))->GetOffsetPos();
 		Vec2 vObjScale = _pOther->GetScale();
 
-		Vec2 vPos = ((CCollider*)GetComponents(Component_TYPE::Collider))->GetFinalPos();
+		Vec2 vPos = GetPos();
+		vPos += ((CCollider*)GetComponents(Component_TYPE::Collider))->GetOffsetPos();
 		Vec2 vScale = ((CCollider*)GetComponents(Component_TYPE::Collider))->GetScale();
 
 		COLLIDER_DIR ColDir = CColliderMgr::GetInst()->CollisionDIR(((CCollider*)pOtherObj->GetComponents(Component_TYPE::Collider)), ((CCollider*)GetComponents(Component_TYPE::Collider)));
@@ -71,7 +74,7 @@ void CGround::OnCollisionEnter(CCollider* _pOther)
 			((CGravity*)pOtherObj->GetComponents(Component_TYPE::Gravity))->SetGround(true); // 충돌 시작시 그라운드 접촉 선언
 
 			pRigid->SetVelocity(Vec2(pRigid->GetVelocity().x, 0.f)); // 땅에 닿아있는 상태라면 0속도를 0으로 셋팅
-			pRigid->SetAccelAlpha(Vec2(0.f, 0.f)); // 추가가속도 삭제
+			pRigid->SetAccelAlpha(Vec2(0.f, 0.f)); // 추가가속도 삭제ㅇ
 
 			float fLen = abs(vObjPos.y - vPos.y);
 			float fValue = (vObjScale.y / 2.f + vScale.y / 2.f) - fLen;
